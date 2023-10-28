263a08bc325c0006a68d626c59ff25ac1c204e41
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-10-28 19:21:07 +0530

    var const final

--- lib/keys/keys.dart
+++ lib/keys/keys.dart
@@ -71,6 +71,8 @@ class _KeysState extends State<Keys> {
               // for (final todo in _orderedTodos) TodoItem(todo.text, todo.priority),
               for (final todo in _orderedTodos)
                 CheckableTodoItem(
+                  // key: ValueKey(todo.text),
+                  key: ObjectKey(todo),
                   todo.text,
                   todo.priority,
                 ),
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,13 @@ import 'package:flutter_internals/ui_updates_demo.dart';
 
 void main() {
   runApp(const App());
+final  numbers = [1, 2, 3];
+ //final - can not override but add 
+ //add , edit ,and override numbers - var 
+ //const - can add but got an error 
+  // numbers = [4, 5, 6]; 
+ 
+  numbers.add(4); 
 }
 
 class App extends StatelessWidget {
