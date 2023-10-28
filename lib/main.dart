9df9fe1b9c5032b6feaa5d79528778c1c7ade518
PURBASHIS BEHERA <108601197+purbashis@users.noreply.github.com>
2023-10-28 11:53:00 +0530

    todo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_internals/keys/keys.dart';
 
 import 'package:flutter_internals/ui_updates_demo.dart';
 
@@ -17,7 +18,7 @@ class App extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter Internals'),
         ),
-        body: const UIUpdatesDemo(),
+        body: const Keys(),
       ),
     );
   }
